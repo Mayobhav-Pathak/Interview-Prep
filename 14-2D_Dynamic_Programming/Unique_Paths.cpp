04b984c4e0ef2777f932fba19f3328335c761449
Mayobhav Pathak <mayobhav.pathak@gmail.com>
2026-08-14 10:24:45 +0530

    Optimal solution for Climbing stairs using Dynamic Programming with O(1) complexity

---
+++ 14-2D_Dynamic_Programming/Unique_Paths.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        if(m==1||n==1) return 1;
+        long int c=1;
+        int k=(m+n-2);
+        if((m-1)>=(n-1)){
+            int l=m;
+        for(int i=1 ; i<n ; i++){
+          c= c*(k-i+1);
+          c /=i;
+        }
+        return (c);
+        }
+        else{
+        int l=n;
+        for(int i=1 ; i<m ; i++){
+          c= c*(k-i+1);
+          c /=i;
+        }
+        return (c);
+        }
+
+    }
+};  
